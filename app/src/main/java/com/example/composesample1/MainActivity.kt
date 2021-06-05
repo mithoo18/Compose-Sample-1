0de517fce161f07bb6e72d6be2a6686bb7c3615f
mits <devanshu18g@gmail.com>
2021-06-06 03:48:46 +0530

    Done & Tested

--- app/src/main/java/com/example/composesample1/MainActivity.kt
+++ app/src/main/java/com/example/composesample1/MainActivity.kt
@@ -1,39 +1,102 @@
 package com.example.composesample1
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composesample1.ui.theme.ComposeSample1Theme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeSample1Theme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
                     loadUI()
-                    Greeting("Android")
                 }
             }
         }
     }
-}
 
+    @Preview
+    @Composable
+    fun loadUI() {
+        Column(modifier = Modifier.padding(5.dp))
+        {
+            Card(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+                    .shadow(5.dp)
+            )
+            {
+                Image(
+                    painter = painterResource(id = R.drawable.img1),
+                    contentDescription = null
+                )
 
-@Composable
-f
-}
+                Text(
+                    text = "First Image",
+                    modifier = Modifier.padding(5.dp),
+                    color = Color.Black,
+                    fontSize = 20.sp
+                )
+            }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeSample1Theme {
-        Greeting("Android")
+            Card(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+                    .shadow(5.dp)
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.img2),
+                    contentDescription = null
+                )
+
+                Text(
+                    text = "Second Image",
+                    modifier = Modifier.padding(5.dp),
+                    color = Color.Black,
+                    fontSize = 20.sp
+                )
+            }
+
+            Card(
+                modifier = Modifier
+                    .padding(10.dp)
+                    .fillMaxWidth()
+                    .shadow(5.dp)
+            ) {
+
+                Image(
+                    painter = painterResource(id = R.drawable.img3),
+                    contentDescription = null
+                )
+
+                Text(
+                    text = "Second Image",
+                    modifier = Modifier.padding(5.dp),
+                    color = Color.Black,
+                    fontSize = 20.sp
+                )
+            }
+        }
     }
-}
